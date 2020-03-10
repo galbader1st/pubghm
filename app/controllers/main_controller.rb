e9592d05cc8d7ab3a57137b98389d8067d7b4f72
galbader <gal.b@phytech.com>
2020-03-10 14:52:44 +0200

    hello world

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,7 @@ class MainController < ApplicationController
   def index
     # byebug
     client = DropboxApi::Client.new(ENV['DROPBOX_RUBY_SDK_ACCESS_TOKEN'])
-    @results = client.list_folder "/recordings/20160820 אסלי"
+    @results = client.list_folder "/recordings/20160709 רן שם טוב"
     @url = client.get_temporary_link(@results.entries.first.path_lower).link
     # create_shared_link_with_settings
     # byebug
