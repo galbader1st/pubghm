f88ad522b79f083a883c9f6ad91acb87e9b809dd
galbader <gal.b@phytech.com>
2020-03-14 12:26:27 +0200

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #config.action_controller.asset_host = "https://pubghm.herokuapp.com/"
+  config.action_controller.asset_host = "https://pubghm.herokuapp.com/"
   # Disable Rails's static asset server (Apache or nginx will already do this)  
   #config.serve_static_assets = false
 
