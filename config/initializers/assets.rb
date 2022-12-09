0a629daab6bf659075e1bb899338411245868b63
galbader <gal.b@phytech.com>
2022-12-09 16:02:32 +0200

    try to remove assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
-Rails.application.config.assets.precompile += %w( jplayer.blue.monday.min.css )
-Rails.application.config.assets.precompile += %w( jplayer.playlist.min.js )
-Rails.application.config.assets.precompile += %w( jquery.jplayer.min.js )
+# Rails.application.config.assets.precompile += %w( jplayer.blue.monday.min.css )
+# Rails.application.config.assets.precompile += %w( jplayer.playlist.min.js )
+# Rails.application.config.assets.precompile += %w( jquery.jplayer.min.js )
